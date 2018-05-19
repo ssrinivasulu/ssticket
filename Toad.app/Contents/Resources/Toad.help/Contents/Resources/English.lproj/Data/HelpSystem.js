73896041a7bdf4d94e451c78d8b3936ec4959622
Santhosh Srinivasulu <santhosh@Santhoshs-Air.fios-router.home>
2018-05-19 11:18:57 -0400

    Remove toad

--- Toad.app/Contents/Resources/Toad.help/Contents/Resources/English.lproj/Data/HelpSystem.js
+++
@@ -1,6 +0,0 @@
-var xmlHelpSystemData = "";
-xmlHelpSystemData += '<?xml version=\"1.0\" encoding=\"utf-8\"?>';
-xmlHelpSystemData += '<WebHelpSystem DefaultUrl=\"Content/Resources/pgs/About_Toad.htm\" Toc=\"Data/Toc.js\" Index=\"Data/Index.js\" Concepts=\"Data/Concepts.xml\" BrowseSequence=\"Data/BrowseSequence.js\" Glossary=\"Data/Glossary.js\" SearchDatabase=\"Data/Search.xml\" Alias=\"Data/Alias.xml\" Synonyms=\"Data/Synonyms.xml\" SearchFilterSet=\"Data/Filters.xml\" SkinName=\"HTML5\" Skins=\"HTML5\" BuildTime=\"04/02/2016 14:31:23\" BuildVersion=\"10.2.2.0\" TargetType=\"WebHelp2\" SkinTemplateFolder=\"Skin/\" InPreviewMode=\"false\" MoveOutputContentToRoot=\"false\" MakeFileLowerCase=\"false\" UseCustomTopicFileExtension=\"false\">';
-xmlHelpSystemData += '    <CatapultSkin Version=\"2\" SkinType=\"WebHelp2\" Comment=\"HTML5 skin\" Anchors=\"Width,Height\" Width=\"800\" Height=\"600\" Top=\"0\" Left=\"0\" Bottom=\"0\" Right=\"0\" Tabs=\"TOC,Index,Community\" DefaultTab=\"TOC\" UseBrowserDefaultSize=\"True\" UseDefaultBrowserSetup=\"True\" Name=\"HTML5\" />';
-xmlHelpSystemData += '</WebHelpSystem>';
-MadCap.Utilities.Xhr._FilePathToXmlStringMap.Add('HelpSystem', xmlHelpSystemData);
--- Toad.app/Contents/Resources/Toad.help/Contents/Resources/English.lproj/Resources/Scripts/require.config.js
+++
@@ -1,3 +0,0 @@
-require.config({
-    urlArgs: 't=635901930839717699'
-});
